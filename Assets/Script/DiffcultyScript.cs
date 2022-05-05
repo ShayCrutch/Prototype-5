c42ead07a2e2ff3e157954e14cb9751b5084508b
ShayCrutch <scrutch@aoiths.org>
2022-05-05 12:42:38 -0400

    Prototype 5.5

---
+++ Assets/Script/DiffcultyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiffcultyScript : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+    public int diffculty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDiffculty);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetDiffculty()
+    {
+        Debug.Log(gameObject.name + " Was Clicked");
+        gameManager.StartGame(diffculty);
+    }
+}
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -15,14 +15,12 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
     public Button restartButton;
+    public GameObject titleScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTargets());
-        score = 0;
-        UpdateScore(0);
+       
         
     }
 
@@ -60,5 +58,15 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int diffculty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTargets());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= diffculty;
     }
 }
